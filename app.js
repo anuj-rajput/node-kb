d59b29c2bf6aceed76132fd7feda5012a9c263d0
Anuj Rajput <anujrajput04@gmail.com>
2018-05-04 09:21:27 +0530

    Templating using Pug

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
 const express = require('express');
+const path = require('path');
+
+// Init App
 const app = express();
 
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home Route
 app.get('/', function(req, res) {
-	res.send('Hello World');
+	let articles = [
+		{
+			id: 1,
+			title: 'Article One',
+			author: 'Anuj Rajput',
+			body: 'This is article one'
+		},
+		{
+			id: 2,
+			title: 'Article Two',
+			author: 'Hari Prasad Chaurasiya',
+			body: 'This is article one'
+		},
+		{
+			id: 3,
+			title: 'Article Three',
+			author: 'Abhishek Upmanyu',
+			body: 'This is article three'
+		},
+	];
+
+	res.render('index', {
+		title: 'Articles',
+		articles: articles
+	});
 });
 
+// Add Route
+app.get('/articles/add', function (req, res) {
+	res.render('add_article', {
+		title: 'Add Article'
+	})
+});
+
+// Start Server
 app.listen(3000, function() {
 	console.log('Server started on port 3000...');
-})
+});
